1180c2e07f99916ef1292498a5fb010bb8f95928
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-02 19:22:05 -0400

    HTTP post requests

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token":"dajkfh23hrdfahdi2uh3uhfafakjd",
+    "username": "ctn",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
 
