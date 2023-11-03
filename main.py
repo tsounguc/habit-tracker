1cfcdae8df033b8bf6bcdf98e2e456e4d0c10d7f
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-02 20:07:07 -0400

    Advanced Authentication using an HTTP Header

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import requests
 
+USERNAME = ""
+TOKEN = ""
+
+# Create user
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token":"dajkfh23hrdfahdi2uh3uhfafakjd",
-    "username": "ctn",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
 
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Create graph
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Running graph",
+    "unit": "mile",
+    "type": "float",
+    "color": "ichou",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+requests.post(url=graph_endpoint, json=graph_config, headers=headers)
